d2d022085b37f6b872f8895623c90b23802e378a
ismoildeveloperr <ismoil.developer@gmail.com>
2025-06-18 16:45:41 +0500

    Installed Node.js and React, learned SPA & JSX, created tech list component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import "./App_style_list.css";
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           ismoil <code   >src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Technologies />
+      <Student />
     </div>
   );
-}
+};
 
+const Technologies = () => {
+  return (
+    <div>
+      <div>Technologies</div>
+      <div className="Technologies">
+        <ul>
+          <li>1. Html</li>
+          <li>2. Css</li>
+          <li>3. Js</li>
+          <li>4. React</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const Student = () => {
+  return (
+    <div>
+      <div>Student course</div>
+      <div className="Student">
+        <ul>
+          <li>1-course</li>
+          <li>2-course</li>
+          <li>3-course</li>
+          <li>4-course</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 export default App;
