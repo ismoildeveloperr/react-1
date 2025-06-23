fa273c3bb53c3ec56fc5ea6a5d7534359d27b39a
ismoildeveloperr <ismoil.developer@gmail.com>
2025-06-23 15:57:14 +0500

    Navlink

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,11 @@ import "./Header.css";
 const Header = () => {
   return (
     <header className="header">
-      <img src="https://e7.pngegg.com/pngimages/708/311/png-clipart-icon-logo-twitter-logo-twitter-logo-blue-social-media-thumbnail.png" />
+     <img
+  src="https://e7.pngegg.com/pngimages/708/311/png-clipart-icon-logo-twitter-logo-twitter-logo-blue-social-media-thumbnail.png"
+  alt="Twitter Logo"
+/>
+
     </header>
   );
 };
--- src/components/Navbar/Nav.jsx
+++ src/components/Navbar/Nav.jsx
@@ -1,23 +1,24 @@
 import React from "react";
 import s from "./Nav.module.css";
+import { NavLink } from "react-router-dom";
 
 const Nav = () => {
   return (
     <nav className={s.nav}>
       <div className={s.item}>
-        <a href="/profile">Profile</a>
+        <NavLink to="/profile">Profile</NavLink>
       </div>
       <div className={`${s.item} ${s.active}`}>
-        <a href="/dialogs">Messages</a>
+        <NavLink to="/dialogs">Messages</NavLink>
       </div>
       <div className={s.item}>
-        <a>News</a>
+        <NavLink>News</NavLink>
       </div>
       <div className={s.item}>
-        <a>Music</a>
+        <NavLink>Music</NavLink>
       </div>
       <div className={s.item}>
-        <a>Settings</a>
+        <NavLink>Settings</NavLink>
       </div>
     </nav>
   );
