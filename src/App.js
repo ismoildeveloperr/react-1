2a1ae01a5bc74d77d9e698aa0cb815259cf58076
ismoildeveloperr <ismoil.developer@gmail.com>
2025-06-24 16:53:33 +0500

    props

--- src/App.js
+++ src/App.js
@@ -1,52 +1,26 @@
-// import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Nav from "./components/Navbar/Nav";
 import Dialogs from "./components/Dialogs/Dialogs";
 import Profile from "./components/Profile/Profile";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-// import Footer from "./Footer";
-// import Technologies from "./Technologies";
-// import Header from "./Header";
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           ismoil <code   >src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-///ismoil ismoil
 const App = () => {
     return (
         <BrowserRouter>
             <div className="app-wrapper">
-                <Header/>
-                <Nav/>
+                <Header />
+                <Nav />
                 <div className="app-wrapper-content">
                     <Routes>
-                        <Route path='/dialogs' element={<Dialogs/>}/>
-                        <Route path='/profile' element={<Profile/>}/>
-                    </ Routes>
+                        <Route path="/dialogs" element={<Dialogs поп/>} />
+                        <Route path="/dialogs/:id" element={<Dialogs />} /> {/* ВАЖНО: добавили это */}
+                        <Route path="/profile" element={<Profile />} />
+                    </Routes>
                 </div>
-                {/*<Profile />*/}
             </div>
         </BrowserRouter>
     );
 };
 
 export default App;
- 
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,91 @@
+// import React from "react";
+// import styles from "./Dialogs.module.css";
+// import { NavLink, useParams } from "react-router-dom";
+
+// const DialogItem = (props) => {
+//     //props.id
+//     let path =  "/dialogs/" + props.id;
+//     return <div className={styles.dialog}>
+//                     <NavLink to={path}>{props.name}</NavLink>
+//                 </div>
+// }
+// const Message = (props) => {
+//     return <div className={styles.dialogs}>{props.messages}</div>
+// }
+// const Dialogs = () => {
+//     const { id } = useParams(); // Получаем ID из URL
+
+//     return (
+//         <div className={styles.dialogs}>
+//             <div className={styles.dialogsItems}>
+//                <DialogItem name ="Исмоил" id ="1"/>
+//                 <DialogItem name = "Фирдавс" id="2"/>
+//                 <DialogItem name = "Фирӯз" id="3"/>
+//                 <DialogItem name = "Шоҳин" id="4"/>
+//             </div>
+//             <div className={styles.messages}>
+//                     <Message messages ="Hi"/>
+//                     <Message messages ="How are you"/>             
+//                     <Message messages ="Good morning"/>             
+//                     <Message messages ="Running"/>             
+
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default Dialogs;
+
 import React from "react";
-import "./Dialogs.module.css"
-const Dialogs = (props) => {
+import styles from "./Dialogs.module.css";
+import { NavLink } from "react-router-dom";
+
+const DialogItem = (props) => {
+    const path = "/dialogs/" + props.id;
     return (
-        <div>
-            Dialogs
+        <div className={styles.dialog}>
+            <NavLink to={path}>{props.name}</NavLink>
         </div>
-    )
-}
+    );
+};
+
+
+const Message = (props) => {
+    return <div className={styles.dialog}>{props.message}</div>;
+};
+
+
+const Dialogs = () => {
+
+    const dialogsData = [
+        { id: "1", name: "Исмоил" },
+        { id: "2", name: "Фирдавс" },
+        { id: "3", name: "Фирӯз" },
+        { id: "4", name: "Шоҳин" },
+    ];
+
+
+    const messagesData = [
+        { id: 1, message: "Hi" },
+        { id: 2, message: "How are you" },
+        { id: 3, message: "Good morning" },
+        { id: 4, message: "Running" },
+    ];
+
+    return (
+        <div className={styles.dialogs}>
+            <div className={styles.dialogsItems}>
+                {dialogsData.map((dialog) => (
+                    <DialogItem key={dialog.id} name={dialog.name} id={dialog.id} />
+                ))}
+            </div>
+            <div className={styles.messages}>
+                {messagesData.map((msg) => (
+                    <Message key={msg.id} message={msg.message} />
+                ))}
+            </div>
+        </div>
+    );
+};
+
 export default Dialogs;
